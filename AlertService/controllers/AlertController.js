297706ce70bfa2507df055fec9ee68a2d6e48120
AshanSalinda <ashansalinda5@gmail.com>
2024-02-23 03:39:42 +0530

    alert backend

---
+++ AlertService/controllers/AlertController.js
@@ -0,0 +1,129 @@
+const { run } = require("googleapis/build/src/apis/run");
+const dataSource = require("../config/config");
+const alertRepo = dataSource.getRepository("Alert");
+
+const getAllAlerts = async (req, res) => {
+    try {
+        if(!req.query.userId){
+            res.status(404).json({message: 'Alerts not found'});
+        }
+
+        else{
+            const alerts = await alertRepo.find({
+                where: {
+                    userId: req.query.userId,
+                    runningStatus: req.query.runningStatus === undefined ? true : req.query.runningStatus,
+                },
+                order: {
+                    coin: 'ASC',
+                    alertId: 'ASC'
+                }
+            });
+            
+            res.status(200).json(alerts);
+        }
+    } 
+    
+    catch (error) {
+        console.log("\nError fetching alerts:", error);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+
+const addAlert = async (req, res) => {
+    try {
+        const newAlert = await alertRepo.save(req.body);
+        const updatedAlerts = await getAllAlerts({ 
+            query: { 
+                userId: req.body.userId,
+                runningStatus: true
+            }
+        }, res );
+        res.status(200).json(updatedAlerts);
+        
+    } 
+    
+    catch (error) {
+        console.log("\nError adding alert:", error);
+        res.status(500).json({message: error.message});
+    }
+};
+
+
+
+const editAlert = async (req, res) => {
+    try {
+        const alertToUpdate = await alertRepo.findOne({
+            where: {
+                alertId: req.query.alertId,
+            },
+        })
+
+        if (!alertToUpdate) {
+            res.status(404).json({message: 'Alert not found'});
+        } 
+        
+        else {
+            alertRepo.merge(alertToUpdate, req.body);
+            await alertRepo.save(alertToUpdate);
+
+            const updatedAlerts = await getAllAlerts({ 
+                query: { 
+                    userId: req.body.userId,
+                    runningStatus: req.query.runningStatus
+                }
+            }, res );
+
+            res.status(200).json(updatedAlerts);
+        }
+    } 
+    
+    catch (error) {
+        console.log("\nError edting alert:", error);
+        res.status(500).json({message: error.message});
+    }
+};
+
+
+
+const deleteAlert = async (req, res) => {
+    try {
+        const alertToDelete = await alertRepo.findOne({
+            where: {
+                alertId: req.query.alertId,
+            },
+        })
+
+        if (!alertToDelete) {
+            res.status(404).json({message: 'Alert not found'});
+        } 
+        
+        else {
+            await alertRepo.remove(alertToDelete);
+
+            const updatedAlerts = await getAllAlerts({ 
+                query: { 
+                    userId: req.query.userId,
+                    runningStatus: req.query.runningStatus
+                }
+            }, res );
+
+            res.status(200).json(updatedAlerts);
+        }
+    } 
+    
+    catch (error) {
+        console.log("\nError deleting alert:", error);
+        res.status(500).json({message: error.message});
+    }
+};
+
+
+module.exports = {
+    getAllAlerts,
+    addAlert,
+    editAlert,
+    deleteAlert
+};
--- AlertService/index.js
+++ AlertService/index.js
@@ -1,20 +1,38 @@
 const express = require("express");
-const dataSource = require("./config/config");
-
+const cors = require('cors');
 const app = express();
+const dataSource = require("./config/config");
+const alertRouter = require("./routes/AlertRoutes");
+
 app.use(express.json());
+app.use(cors());
+app.use("/alert", alertRouter);
 
-const userRouter = require("./routes/UserRoutes");
+app.use((req, res) => {
+    console.log(`${req.originalUrl} Endpoint Not found`);
+    res.status(404).json({
+        message: `${req.originalUrl} Endpoint Not found`
+    });
+});
 
-app.use("/user",userRouter);
+app.use((error, req, res) => {
+    console.log("Error :", error);
+    res.status(500).json({
+        message: error.message
+    });
+});
 
-dataSource.initialize().then(() => {
+
+dataSource.initialize()
+
+.then(() => {
     console.log("Database connected!!");
 
     app.listen(8081, () => {
-        console.log("Server Started on Port 8081")
+        console.log("Server Started on Port 8081");
     })
 })
-    .catch((err) => {
-        console.log(err)
-    })
+
+.catch((err) => {
+    console.log(err);
+})
