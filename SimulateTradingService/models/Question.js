9c7a211a317d08f38807e5b6cc85912822cefca4
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-03-05 20:04:26 +0530

    Question model change

--- SimulateTradingService/models/Question.js
+++ SimulateTradingService/models/Question.js
@@ -25,7 +25,7 @@ module.exports = new EntitySchema({
             type: "varchar",
         },
         correct_answer: {
-            type: "varchar",
+            type: "int",
         },
     },
 })
