a3df0dfb48a4cd1e31cfcd8f0de615df91e86fc6
Divya803 <palpolatbd.21@uom.lk>
2024-03-26 20:58:53 +0530

    Get Data for verify requests

--- UserService/controllers/UserController.js
+++ UserService/controllers/UserController.js
@@ -35,9 +35,38 @@ const deleteUser = async (req, res) => {
     }
 };
 
+const getUserCount = async (req, res) => {
+    const userRepo = dataSource.getRepository("User");
+    try {
+        const userCount = await userRepo.count();
+        res.json({ count: userCount });
+    } catch (error) {
+        console.error("Error fetching user count:", error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+
+const getPendingUsers = async (req, res) => {
+    const userRepo = dataSource.getRepository("User");
+    try {
+        const pendingUsers = await userRepo.find({
+            where: {
+                Verified: "Pending"
+            }
+        });
+        res.json(pendingUsers);
+    } catch (error) {
+        console.error("Error retrieving pending users:", error);
+        res.status(500).json({message: 'Internal server error'});
+    }
+};
+
 
 module.exports = {
     getAllUsers,
     saveUser,
-    deleteUser
+    deleteUser,
+    getUserCount,
+    getPendingUsers
 }
