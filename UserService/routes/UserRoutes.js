8ded1bd2783020af18b77b8fa2cb7ae3c83e68a2
Divya803 <palpolatbd.21@uom.lk>
2024-03-26 16:54:12 +0530

    Adding Data for User Table

--- UserService/routes/UserRoutes.js
+++ UserService/routes/UserRoutes.js
@@ -2,9 +2,9 @@ const express = require('express');
 const controller = require("../controllers/UserController");
 const router = express.Router();
 
-router.get("/", controller.getAllUsers);
+router.get("/getAllUsers", controller.getAllUsers);
 
-router.post("/", controller.saveUser);
+router.post("/saveUser", controller.saveUser);
 
 router.delete("/:id", controller.deleteUser);
 
