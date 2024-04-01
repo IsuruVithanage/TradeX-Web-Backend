e9688ef58f1485090c02be615508b7964e8961f8
AshanSalinda <ashansalinda5@gmail.com>
2024-04-02 01:27:06 +0530

    updated

--- PortfolioService/Scheduler/scheduler.js
+++ PortfolioService/Scheduler/scheduler.js
@@ -1,28 +1,25 @@
-const schedule = require('node-schedule');
+const cron = require('node-cron');
 const updatePortfolioValueOf = require("../controllers/PortfolioValueController").updatePortfolioValueOf;
 
 const runScheduledValueUpdaters = () => {
     
-    schedule.scheduleJob('0 * * * * *', async (fireDate) => {
+    cron.schedule('0 * * * * *', async () => {
         await updatePortfolioValueOf("Hourly");
-        console.log("\x1b[33mPortfolio Hourly Value updated\x1b[0m at:", fireDate.toLocaleString(), "\n")
-
+        console.log("\x1b[33mPortfolio Hourly Value updated\x1b[0m at:", new Date().toLocaleString(), "\n")
     });
 
 
 
-    schedule.scheduleJob('5 */2 * * * *', async (fireDate) => {
+    cron.schedule('5 */2 * * * *', async () => {
         await updatePortfolioValueOf("Daily");
-        console.log("\x1b[33mPortfolio Daily Value updated\x1b[0m at:", fireDate.toLocaleString(), "\n")
-
+        console.log("\x1b[33mPortfolio Daily Value updated\x1b[0m at:", new Date().toLocaleString(), "\n")
     });
 
 
 
-    schedule.scheduleJob('10 */3 * * * *', async (fireDate) => {
+    cron.schedule('10 */3 * * * *', async () => {
         await updatePortfolioValueOf("Weekly");
-        console.log("\x1b[33mPortfolio Weekly Value updated\x1b[0m at:", fireDate.toLocaleString(), "\n")
-
+        console.log("\x1b[33mPortfolio Weekly Value updated\x1b[0m at:", new Date().toLocaleString(), "\n")
     });
 };
 
