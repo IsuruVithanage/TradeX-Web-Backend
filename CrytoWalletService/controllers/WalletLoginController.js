f08c02e55499248712bfed87442bcc6d1b680b47
Your Name <you@example.com>
2024-04-10 15:47:15 +0530

    password hashing

--- CrytoWalletService/controllers/WalletLoginController.js
+++ CrytoWalletService/controllers/WalletLoginController.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const dataSource = require("../config/config");
 const bcrypt = require('bcrypt')
+const userRepo = dataSource.getRepository("UserDetail");
+
 
 const login = async (req, res) => {
-    res.json("login");
+    const usersave = userRepo.save(req.body);
+    res.json(usersave);
 
 };
 
 const register = async (req, res) => {
     const {username,password} = req.body;
-    console.log(req.body)
-    bcrypt.hash(password,10).then((hash) => {
-        console.log(hash)
-        res.status(200).json({"hash": hash, "password": password})
+    
+    bcrypt.hash(password,10).then(async(hash) => {
+        const user = await userRepo.findOne({
+            where: {
+                "userName": username
+            }
+        })
+
+        if(user){
+            return res.status(400).json({"message": "User Already existing"})
+        }
+
+        userRepo.save({
+            "userName": username, 
+            "password": hash
+        }).then(()=>{
+            console.log(hash)
+            res.status(200).json({"hash": hash, "password": password})
+        }).catch((error)=>{
+            console.log(error)
+            res.status(400).json(error);
+  
+        })
     }).catch((error)=>{
         console.log(error)
         res.status(400).json(error);
