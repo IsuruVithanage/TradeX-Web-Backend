e0f2728d11499ea0576b5e68cdc11fc3f2950efa
Inupama21 <kavisekarais.21@uom.lk>
2024-04-29 19:52:14 +0530

    forum-answers

--- ForumService/routes/ForumRoutes.js
+++ ForumService/routes/ForumRoutes.js
@@ -6,6 +6,7 @@ router.get("/getAllQuestions", controller.getAllQuestions);
 
 router.get("/getQuestionsByUserId/:userId", controller.getQuestionsByUserId);
 
+router.get("/getQuestionsByQuestionId/:questionId", controller.getQuestionsByQuestionId);
 router.post("/", controller.saveQuestion);
 
 router.delete("/:id", controller.deleteQuestion);
