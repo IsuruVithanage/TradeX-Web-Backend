203720dcf18d1dfbc6af7bb32b276daa3064ad5f
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-06-12 03:23:07 +0530

    Add refresh token expire update

--- UserService/JWT.js
+++ UserService/JWT.js
@@ -7,7 +7,7 @@ const createAccessToken = (user) => {
   return jwt.sign(
       { id: user.userId, userName: user.userName, roles: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: '2m' }
+      { expiresIn: '15m' }
   );
 };
 
@@ -15,7 +15,7 @@ const createRefreshToken = (user) => {
   return jwt.sign(
       { id: user.userId },
       process.env.JWT_REFRESH_SECRET,
-      { expiresIn: '3m' }
+      { expiresIn: '30d' }
   );
 };
 
