e7866b37719d5b8f025fa4e0f91bc0b15237c45a
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-06-12 03:21:00 +0530

    Add refresh token update

--- UserService/JWT.js
+++ UserService/JWT.js
@@ -7,7 +7,7 @@ const createAccessToken = (user) => {
   return jwt.sign(
       { id: user.userId, userName: user.userName, roles: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: '5m' } // Short-lived access token (e.g., 15 minutes)
+      { expiresIn: '2m' }
   );
 };
 
@@ -15,7 +15,7 @@ const createRefreshToken = (user) => {
   return jwt.sign(
       { id: user.userId },
       process.env.JWT_REFRESH_SECRET,
-      { expiresIn: '30d' } // Long-lived refresh token (e.g., 30 days)
+      { expiresIn: '3m' }
   );
 };
 
