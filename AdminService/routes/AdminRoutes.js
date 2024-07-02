8e1e89d7d3bd01588c3b577e0bac70e84902a73d
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-07-03 00:37:06 +0530

    Quiz Update

--- AdminService/routes/AdminRoutes.js
+++ AdminService/routes/AdminRoutes.js
@@ -6,6 +6,8 @@ router.get("/getAllAdmins", controller.getAllAdmins);
 
 router.post("/saveAdmin", controller.saveAdmin);
 
+router.post("/login", controller.login);
+
 router.delete("/:id", controller.deleteAdmin);
 
 router.get("/getAdminCount", controller.getAdminCount);
